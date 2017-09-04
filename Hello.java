4c3af17bfc3de5fc12405a46a6b026e0f9ceab41
wuguojun119 <wuguojun119@126.com>
2017-09-04 16:41:38 +0800

    修改Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
 	public static void main(Stirng[] args){
 		System.out.println("Hello World !");
+		System.out.println("wuguojun119");
+		System.out.println("wuguojun119@126.com");
 	}
 }
